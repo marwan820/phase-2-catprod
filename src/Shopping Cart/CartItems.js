8c587eddd88fafe461dbff216ec9cb90127a9bb9
Marwan Rahim <marwan820@Marwan>
2023-10-25 19:29:41 -0500

    added dollar sign $ in front of item price in CartItems

--- src/Shopping Cart/CartItems.js
+++ src/Shopping Cart/CartItems.js
@@ -9,7 +9,7 @@ const CartItems = ({ item,deleteFromLocalStorage }) => {
    <Card.Body key={item.id} className="cart-card" >
       <Card.Img src={item.image} alt={item.name} />
       <Card.Text>{item.name}</Card.Text>
-      <Card.Text>{item.price}</Card.Text>
+      <Card.Text>${item.price}</Card.Text>
       <Button onClick={() => deleteFromLocalStorage(item.id)}>Remove from cart</Button>
       </Card.Body>
     </Card>
